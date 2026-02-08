a9eb7a40197a3b324580562ca2c53fa8c5c511b1
Loran Maes <loran.maes@aware.be>
2026-02-08 17:42:36 +0100

    Fix, CUD actions training session as athlete

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,17 @@ use App\Http\Controllers\Api\ActivityController;
 use App\Http\Controllers\Api\TrainingPlanController;
 use App\Http\Controllers\Api\TrainingSessionController;
 use App\Http\Controllers\Api\TrainingWeekController;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware([StartSession::class, 'auth'])->group(function (): void {
+Route::middleware([
+    EncryptCookies::class,
+    AddQueuedCookiesToResponse::class,
+    StartSession::class,
+    'auth',
+])->group(function (): void {
     Route::apiResource('training-plans', TrainingPlanController::class);
     Route::apiResource('training-weeks', TrainingWeekController::class);
     Route::apiResource('training-sessions', TrainingSessionController::class);
